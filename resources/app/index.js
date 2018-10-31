8e38b9ef9162ad784eb41353907cd394e82ef310
Jitender Bhutani <Jitender.Bhutani@AuxesisGroup.com>
2018-10-31 18:32:11 +0530

    Updated icons

--- resources/app/index.js
+++ resources/app/index.js
@@ -1,13 +1,15 @@
-const {app, BrowserWindow, nativeImage} = require('electron') 
-const electron = require('electron')
-const ipcMain= electron.ipcMain
+const {app, BrowserWindow} = require('electron') 
 const url = require('url') 
 const path = require('path')
 
 let win
 
 function createWindow() { 
-	win = new BrowserWindow({width: 1000,height: 600})
+	win = new BrowserWindow({
+		width: 1000,
+		height: 600,
+		icon: path.join(__dirname, '/src/icons/3.png')
+	})
 	win.loadURL(url.format ({ 
 	  pathname: path.join(__dirname, '/src/index.html'), 
 	  protocol: 'file:', 
